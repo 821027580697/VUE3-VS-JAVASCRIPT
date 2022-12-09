62e35d777d583645e08ee825c94542b4a2158677
Anit <suyong0697@nate.com>
2022-12-09 17:09:50 +0900

    Feat : Vue3 how to use CompositionAPI, Setup,  template, Reactivity half

---
+++ learn-vue3/src/lectures/4/App_CompositionAPI.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>반응형 메세지</h2>
+    <p>{{ reactiveMessage }}</p>
+    <button v-on:click="addReactiveMessage">Add Message</button>
+    <h2>일반 메세지</h2>
+    <p>{{ nomalMessage }}</p>
+    <button v-on:click="addNomalMessage">Add Message</button>
+  </div>
+</template>
+
+<script>
+import {isRef, onBeforeMount, onMounted, ref} from "vue";
+
+export default {
+  setup() {
+    console.log("setup");
+    const reactiveMessage = ref("Hello Reactive Message");
+    const addReactiveMessage = () => {
+      reactiveMessage.value = reactiveMessage.value + "!";
+    };
+
+    console.log("isRef(reactiveMessage) : ", isRef(reactiveMessage));
+    let nomalMessage = "Hello Normal Message";
+    const addNomalMessage = () => {
+      nomalMessage = nomalMessage + "!";
+    };
+    console.log("isRef(nomalMessage) : ", isRef(nomalMessage));
+
+    onMounted(() => {
+      console.log("onMounted");
+    });
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    });
+    return {
+      reactiveMessage,
+      nomalMessage,
+      addReactiveMessage,
+      addNomalMessage,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ learn-vue3/src/lectures/6/App_template.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2>보간법</h2>
+    <p>문자열 : {{ message }}</p>
+    <p v-once> {{ message }}</p>
+    <button v-on:click="message = message + '!'">click!</button>
+    <hr/>
+    <h2>v-html</h2>
+    <p>{{ rawHtml }}</p>
+    <p v-html="rawHtml"></p>
+    <hr>
+    <div :title="dynamicTitle">마우스를 올려보세요</div>
+    <input type="text" value="송수용" :disabled="isInputDisabled">
+    <input v-bind="attrs">
+    <h2>javascript</h2>
+    {{ message.split('').reverse().join('')}}<br />
+    {{ isInputDisabled ? '예' : '아니오'}}
+
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  setup() {
+    const message = ref('안녕하세요!');
+    const rawHtml = ref('<strong>안녕하세요</strong>')
+
+    const dynamicTitle = ref('안녕!')
+    const isInputDisabled = ref(true)
+    const attrs = ref({
+      type:'password',
+      value:'12345678',
+      disabled:false
+    })
+    return {
+      message,
+      rawHtml,
+      isInputDisabled,
+      dynamicTitle,
+      attrs
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
