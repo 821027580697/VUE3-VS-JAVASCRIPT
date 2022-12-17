00df486f9787cbaf36d5395de19976a5bcebdfeb
Anit <suyong0697@nate.com>
2022-12-17 13:51:57 +0900

    Vue3 use to v-for, v-if, cloak, directive tag lectures & study

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,13 +1,28 @@
+
+
 <template>
   <div>
-
+    <p>{{ msg }}</p>
+    <p v-text="msg"></p>
+    <p v-html="htmlStr"></p>
+    <p v-text="htmlStr"></p>
+    <p v-pre>{{ msg }}</p>
+    <p>{{ htmlStr}}</p>
+    <p v-once>{{msg}}!!!!!</p>
   </div>
 </template>
 
 <script>
+import {ref} from "vue";
+
 export default {
   setup() {
-
+    const msg = ref('안녕하세요')
+    const htmlStr = ref('<strong>안녕</strong>')
+    return {
+      msg,
+      htmlStr
+    }
   }
 }
 </script>
@@ -15,3 +30,19 @@ export default {
 <style scoped>
 
 </style>
+<!--#디렉티브-->
+<!--v-text-->
+<!--v-html-->
+<!--v-show-->
+<!--v-if-->
+<!--v-else-->
+<!--v-else-if-->
+<!--v-for-->
+<!--v-on-->
+<!--v-bind-->
+<!--v-model-->
+<!--v-slot-->
+<!--v-pre-->
+<!--v-once-->
+<!--v-cloak-->
+<!--v-memo-->
---
+++ learn-vue3/src/lectures/11/App_v-for.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <ul>
+      <template v-for="(item, index) in evenItems" :key="item.id">
+        <!--        <li v-if="item.id % 2 === 0">-->
+        <li v-if="item.id % 2 === 0">
+          ID: {{ item.id }},
+          인덱스: {{ index }},
+          {{ item.message}}
+        </li>
+      </template>
+    </ul>
+    <hr/>
+    <ul>
+      <li v-for="(value, key, index) in myObject" :key="key">
+        {{ index }} - {{ key }} - {{ value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {computed, reactive} from "vue";
+
+export default {
+  setup() {
+    const items = reactive([
+      { id: 1, message: "Java" },
+      { id: 2, message: "HTML" },
+      { id: 3, message: "CSS" },
+      { id: 4, message: "Javascript" },
+    ]);
+
+    const evenItems = computed(() => items.filter(item => item.id % 2 === 0))
+
+    const myObject =reactive({
+      title:'제목',
+      author:'홍길동',
+      published:'2015-12-13'
+    })
+    return {
+      items,
+      evenItems,
+      myObject
+    };
+  },
+};
+</script>
+
+<style scoped></style>
+<!--#for 랜더링-->
+<!-- v-for와 v-if를 동시에 사용하는 것은 좋지 않다.-->
+<!--그래서 template를 사용하여 한번 덮어주고, 그 안에 있는-->
+<!--태그에 v-if를 활용한다.-->
+<!--<li v-for="( value, key, index) in myObject" :key="key">-->
+<!--{{ key }} - {{ value }} - {{ index }}-->
+<!--</li>-->
